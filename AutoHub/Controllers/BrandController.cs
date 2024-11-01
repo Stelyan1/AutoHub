39766f8dda0200c228ac77662707fd14f2bcd6af
Stelyan1 <stilqnbliznakov@gmail.com>
2024-11-01 12:11:35 +0200

    Created BrandController,Created simple view for the brand to show all brands which is index.cshtml and tried to add image didn't work :)

---
+++ AutoHub/Controllers/BrandController.cs
@@ -0,0 +1,38 @@
+﻿using AutoHub.Data;
+using AutoHub.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoHub.Controllers
+{
+    public class BrandController : Controller
+    {
+        private readonly AutoHubDbContext dbContext;
+
+        public BrandController(AutoHubDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Brand> allBrands = this.dbContext
+                .Brands
+                .ToList();
+
+            return View(allBrands);
+        }
+
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Brand brand) 
+        {
+            return View(brand);
+        }
+    }
+}
