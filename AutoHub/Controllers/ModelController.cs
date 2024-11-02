d02632dca383c4061e317cf253dd55907be8b2dc
Stelyan1 <stilqnbliznakov@gmail.com>
2024-11-02 15:47:08 +0200

    Created ModelController to handle Index page and Created the Index view

---
+++ AutoHub/Controllers/ModelController.cs
@@ -0,0 +1,26 @@
+﻿using AutoHub.Data;
+using AutoHub.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoHub.Controllers
+{
+    public class ModelController : Controller
+    {
+        private readonly AutoHubDbContext dbContext;
+
+        public ModelController(AutoHubDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Model> allModels = this.dbContext
+                .Models
+                .ToList();
+
+            return View(allModels);
+        }
+    }
+}
