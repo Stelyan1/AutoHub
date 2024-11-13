9937ba7114559c54a4b559ec577b5ff9319d4002
Stelyan1 <stilqnbliznakov@gmail.com>
2024-11-13 14:12:28 +0200

    Added again ProductController and Index.cshtml plus Create.cshtml and ViewModel

---
+++ AutoHub.Web.ViewModels/Models/ProductViewModel.cs
@@ -0,0 +1,51 @@
+﻿using AutoHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoHub.Web.ViewModels.Models
+{
+    using static AutoHub.Common.EntityValidationConstants.Product;
+    using static AutoHub.Common.ProductModelErrorMessages;
+    public class ProductViewModel
+    {
+        [Required(ErrorMessage = ProductNameErrorMsg)]
+        [MinLength(ProductNameMinLength)]
+        [MaxLength(ProductNameMaxLength)]
+        public string ProductName { get; set; } = null!;
+
+        [Required(ErrorMessage = ManufacturerErrorMsg)]
+        [MinLength(ManufacturerMinLength)]
+        [MaxLength(ManufacturerMaxLength)]
+        public string Manufacturer { get; set; } = null!;
+
+        [Required(ErrorMessage = CarsApplicationErrorMsg)]
+        [MinLength(CarsApplicationMinLength)]
+        [MaxLength(CarsApplicationMaxLength)]
+        public string CarsApplication { get; set; } = null!;
+
+        [Required(ErrorMessage = DescriptionErrorMsg)]
+        [MinLength(DescriptionMinLength)]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        [Required(ErrorMessage = PriceErrorMsg)]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = ImageUrlErrorMsg)]
+        public string ImageUrl { get; set; } = null!;
+
+        //[Required(ErrorMessage = SellerErrorMsg)]
+        //public string SellerId { get; set; } = null!;
+
+        [Required(ErrorMessage = AddedOnErrorMsg)]
+        public DateTime AddedOn { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = CategoryErrorMsg)]
+        public Guid CategoryId { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
+            = new HashSet<Category>();
+    }
+}
---
+++ AutoHub/Controllers/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using AutoHub.Data;
+using AutoHub.Data.Models;
+using AutoHub.Web.ViewModels.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AutoHub.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AutoHubDbContext dbContext;
+
+        public ProductController(AutoHubDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Index()
+        {
+            IEnumerable<Product> allProducts = this.dbContext
+                .Products.ToList();
+
+            return View(allProducts);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Create() 
+        {
+            var categories = this.dbContext
+                .Categories.ToList();
+
+            var productViewModel = new ProductViewModel
+            {
+                Categories = categories
+            };
+
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Create(ProductViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var products = new Product
+                {
+                    ProductName = model.ProductName,
+                    Manufacturer = model.Manufacturer,
+                    CategoryId = model.CategoryId,
+                    CarsApplication = model.CarsApplication,
+                    Description = model.Description,
+                    Price = model.Price,
+                    ImageUrl = model.ImageUrl,
+                    SellerId = GetSellerId() ?? string.Empty,
+                    AddedOn = model.AddedOn
+                };
+
+                dbContext.Products.Add(products);
+                dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            model.Categories = dbContext.Categories.ToList();
+
+            return View(model);
+        }
+
+        private string GetSellerId()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
